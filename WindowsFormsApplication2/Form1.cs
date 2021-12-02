1355ba8273704f7e4d3ffb68e4a8125b509a9c59
Berat Taş 84 <92547735+beratas0084@users.noreply.github.com>
2021-12-02 12:03:51 +0300

    ClassYapısı

---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class Kado
+        {
+            private int a, b;
+
+            public Kado(int a, int b)
+            { 
+            this.a =a;
+            this.b = b;
+            }
+
+            public int topla() {
+                return this.a + this.b;
+            } 
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Kado KadoClass = new Kado(10, 20);
+            MessageBox.Show(KadoClass.topla().ToString());
+        }
+    }
+}
